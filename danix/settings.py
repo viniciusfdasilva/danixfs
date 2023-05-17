87443c7bb429344c544be8645e9c1e58c6ea7a7b
Vinicius <vinicius@pop-os.localdomain>
2023-05-17 00:00:39 -0300

    Set instruction to fix python registry.py file error

--- danix/settings.py
+++ danix/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'danix/db/db.sqlite3',
     }
 }
 
